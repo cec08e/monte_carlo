5369af410a69cac4d19082754c97d3fc6c501b39
Caitlin Carnahan <caitlin@Caitlins-MBP.wv.cc.cmu.edu>
2018-03-27 15:48:25 -0400

    Finally mixing C MC code and Python plotting func

--- code/heisenberg3d.c
+++ code/heisenberg3d.c
@@ -6,21 +6,28 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_sf_exp.h>
+#include <time.h>
 
 /*
 Requirements: GNU Scientific Library (gsl) and CBLAS
 
 gcc -lgsl -lgslcblas heisenberg3d.c
+ALT: gcc -fPIC -shared -o heisenberg3d.so -lgsl -lgslcblas heisenberg3d.c
+
+sudo gdb python3
+run plot_h3d.py
 */
 
 #define ROWS 10       /* Number of rows in each lattice layer */
 #define COLS 10       /* Number of columns in each lattice layer */
-#define RADIUS .4     /* Radius of tangent disc in perturbing function */
-#define K1 -1
-#define K2 -1
+#define RADIUS 5    /* Radius of tangent disc in perturbing function */
 #define J_INTRA 1
 #define J_INTER 1
-#define B_EXT 1
+#define INIT_T 5
+
+double B_EXT = 1;
+double K1 = -1;
+double K2 = -1;
 
 typedef struct {
   double x;
@@ -38,14 +45,19 @@ typedef struct {
 typedef spin_t lattice_t[2][ROWS][COLS];
 
 gsl_rng * rng;
+lattice_t lattice;
 
 
-void initialize_lattice(lattice_t);
+void initialize_lattice();
 void gen_random_spin(spin_t*);
-void simulate(lattice_t, int, double);
-void sweep(lattice_t, double);
+void simulate( int, double);
+int sweep( double);
 void perturb_spin(spin_t* , spin_t*);
-double calc_delta_E(lattice_t, spin_t* , spin_t*, int, int, int);
+double calc_delta_E( spin_t* , spin_t*, int, int, int);
+double calc_magnetization( int);
+void M_v_B();
+void mag_v_temp(double, double, double, int, int);
+
 
 
 
@@ -54,43 +66,63 @@ int main(){
 
   int rows, cols;
   double J_intra, J_inter, B, k1, k2, init_T;
-  lattice_t lattice;
+  //lattice_t lattice;
 
-  rng = gsl_rng_alloc(gsl_rng_mt19937);
-  gsl_rng_set (rng, 0);
+  //rng = gsl_rng_alloc(gsl_rng_mt19937);
+  //gsl_rng_set (rng, 0);
 
+  clock_t begin = clock();
 
-  initialize_lattice(lattice);
-  simulate(lattice, 1, .1);
+  initialize_lattice();
+  M_v_B();
+  //mag_v_temp(lattice, 5.0, .01, .05, 10000, 5000);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Execution time: %f \n", time_spent);
 
 }
 
-void initialize_lattice(lattice_t lattice){
+void initialize_lattice(){
   int i, j, k;
+
+  rng = gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set (rng, 0);
+  //printf("In initialize_lattice\n");
   for(i = 0; i < ROWS; i++){
     for(j = 0; j < COLS; j++){
       gen_random_spin(&lattice[0][i][j]);
       gen_random_spin(&lattice[1][i][j]);
     }
   }
+  //printf("end initialize_lattice\n");
+
 }
 
 void gen_random_spin(spin_t* spin){
     double x1, x2, mag_sq;
+    gsl_rng_uniform(rng);
+    //printf("In gen_random_spin\n");
+
 
     x1 = gsl_rng_uniform(rng)*2.0 - 1.0;
     x2 = gsl_rng_uniform(rng)*2.0 - 1.0;
     mag_sq = gsl_pow_2(x1) + gsl_pow_2(x2);
+    //fprintf(stderr, "In gen_random_spin 2\n");
+
 
     while(mag_sq >= 1){
       x1 = gsl_rng_uniform(rng)*2.0 - 1.0;
       x2 = gsl_rng_uniform(rng)*2.0 - 1.0;
       mag_sq = gsl_pow_2(x1) + gsl_pow_2(x2);
     }
+    //fprintf(stderr,"In gen_random_spin 3\n");
 
     spin->x = 2.0*x1*sqrt(1.0-mag_sq);
     spin->y = 2.0*x2*sqrt(1.0-mag_sq);
     spin->z = 1.0-2.0*mag_sq;
+
+    //fprintf(stderr, "end gen_random_spin\n");
 
     //printf("x: %f\n", spin->x);
     //printf("y: %f\n", spin->y);
@@ -98,15 +130,16 @@ void gen_random_spin(spin_t* spin){
 
 }
 
-void simulate(lattice_t lattice, int num_sweeps, double T){
-  int i;
+void simulate(int num_sweeps, double T){
+  int i, num_accept = 0;
   for(i = 0; i < num_sweeps; i++){
-    sweep(lattice, T);
+    num_accept += sweep( T);
   }
+  printf("Acceptance ratio: %f \n", num_accept/(num_sweeps*ROWS*COLS*2.0));
 }
 
-void sweep(lattice_t lattice, double T){
-  int i,num;
+int sweep(double T){
+  int i, num_accept = 0;
   int layer, row, col;
   double delta_E;
   double random_num;
@@ -117,28 +150,25 @@ void sweep(lattice_t lattice, double T){
     layer = gsl_rng_get(rng) % 2;
     row = gsl_rng_get(rng) % ROWS;
     col = gsl_rng_get(rng) % COLS;
-    printf("Chosen site on layer %d: (%d,%d)\n", layer, row, col);
-    /*
-    if(layer == 0 && row == 0 && col == 7){
-      scanf("%d", &num);
 
-    }
-    */
 
     /* Generate a perturbation of the spin at the chosen lattice site */
     perturb_spin(&temp_spin, &lattice[layer][row][col]);
-    delta_E = calc_delta_E(lattice, &temp_spin, &lattice[layer][row][col], layer, row, col);
+    delta_E = calc_delta_E(&temp_spin, &lattice[layer][row][col], layer, row, col);
     random_num = gsl_rng_uniform(rng);
-    printf("random number: %f\n", random_num);
+    //printf("random number: %f\n", random_num);
     if( !( (delta_E > 0) && (random_num >= gsl_sf_exp(-(1.0/T)*delta_E)) ) ){
       lattice[layer][row][col].x = temp_spin.x;
       lattice[layer][row][col].y = temp_spin.y;
       lattice[layer][row][col].z = temp_spin.z;
-      printf("Accepted. \n");
+      num_accept += 1;
+      //printf("Accepted. \n");
     }
 
 
   }
+
+  return num_accept;
 
 }
 
@@ -213,7 +243,7 @@ void perturb_spin(spin_t* temp_spin, spin_t* spin){
   temp_spin->y = gsl_vector_get(temp_vector, 1);
   temp_spin->z = gsl_vector_get(temp_vector, 2);
 
-  printf("Generated perturbation: %f, %f, %f \n", temp_spin->x, temp_spin->y, temp_spin->z);
+  //printf("Generated perturbation: %f, %f, %f \n", temp_spin->x, temp_spin->y, temp_spin->z);
 
   gsl_vector_free(spin_vector);
   gsl_vector_free(point_vector);
@@ -221,7 +251,7 @@ void perturb_spin(spin_t* temp_spin, spin_t* spin){
   gsl_matrix_free(rot_matrix);
 }
 
-double calc_delta_E(lattice_t lattice, spin_t* temp_spin, spin_t* spin, int layer, int row, int col){
+double calc_delta_E(spin_t* temp_spin, spin_t* spin, int layer, int row, int col){
 
   double delta_dot_neighbor;
   double delta_dot_inter;
@@ -280,10 +310,100 @@ double calc_delta_E(lattice_t lattice, spin_t* temp_spin, spin_t* spin, int laye
   */
 
   delta_E = -J_INTRA*delta_dot_neighbor + J_INTER*delta_dot_inter + delta_a - B_EXT*gsl_vector_get(delta_vector,2);
-  printf("Delta E is %f \n", delta_E);
+  //printf("Delta E is %f \n", delta_E);
+
+  gsl_vector_free(delta_vector);
+  gsl_vector_free(neighbor_vector);
+  gsl_vector_free(inter_vector);
 
   return delta_E;
 
+}
 
+void cool_lattice(double T){
+    float curr_temp;
+    int num_sweeps;
+    curr_temp = INIT_T;
+    while(curr_temp > T){
+        curr_temp -= .02;
+        printf("Cooling to %f \n", curr_temp);
+        simulate(15000, curr_temp);
+    }
+}
 
+double calc_magnetization(int layer){
+      float mag, mag_spin;
+      int i,j,k;
+      mag = 0.0;
+      mag_spin = 0.0;
+      if(layer == -1){
+        for(i=0; i < 2; i++)
+            for(j=0; j < ROWS; j++)
+                for(k = 0; k < COLS; k++)
+                    mag += lattice[i][j][k].z;
+        mag_spin = mag/(ROWS*COLS*2);
+      }
+      else{
+        for(j=0; j < ROWS; j++)
+            for(k = 0; k < COLS; k++)
+                mag += lattice[layer][j][k].z;
+        mag_spin = mag/(ROWS*COLS);
+      }
+      printf("Magnetization per spin (layer %d) is %f \n", layer, mag_spin);
+
+      return mag_spin;
+}
+
+void M_v_B(){
+    double results[202][4];
+    int sample_counter = 0;
+    B_EXT = -5.0;
+    cool_lattice(.1);
+    while(B_EXT < 5.0){
+        printf("B: %f\n", B_EXT);
+        simulate(15000, .1);
+        // Measure magnetization
+        results[sample_counter][0] = B_EXT;
+        results[sample_counter][1] = calc_magnetization( -1);
+        results[sample_counter][2] = calc_magnetization( 0);
+        results[sample_counter][3] = calc_magnetization( 1);
+        sample_counter += 1;
+
+        B_EXT += .1;
+    }
+
+    while(B_EXT > -5.0){
+      printf("B: %f\n", B_EXT);
+        simulate(15000, .1);
+        // Measure magnetization
+        results[sample_counter][0] = B_EXT;
+        results[sample_counter][1] = calc_magnetization( -1);
+        results[sample_counter][2] = calc_magnetization( 0);
+        results[sample_counter][3] = calc_magnetization( 1);
+        sample_counter += 1;
+        B_EXT -= .1;
+    }
+}
+
+void mag_v_temp( double init_temp, double final_temp, double temp_step, int eq_time, int cor_time){
+
+      double curr_temp = init_temp;
+      double mag, mag1, mag2;
+
+      while(curr_temp > final_temp){
+          curr_temp -= temp_step;
+          simulate( eq_time, curr_temp);
+
+          simulate( cor_time, curr_temp);
+
+          mag = calc_magnetization( -1);  // Mag
+          mag1 = calc_magnetization( 0);  // Mag1
+          mag2 = calc_magnetization( 1);  // Mag2
+
+          printf("Current temp: %f \n", curr_temp);
+          printf("Magnetization (total): %f \n", mag);
+          printf("Magnetization (layer1): %f \n", mag1);
+          printf("Magnetization (layer2): %f \n\n", mag2);
+      }
+      // **************Underflow error generated - check it out.
 }
---
+++ code/plot_h3d.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import ctypes
+
+_h3d = ctypes.CDLL('heisenberg3d.so')
+_h3d.initialize_lattice.argtypes = ()
+_h3d.M_v_B.argtypes = ()
+
+def mag_v_temp(init_temp = 0, final_temp = 1, temp_step = .1, eq_time = None, cor_time = None):
+    ''' Plots the mean magnetization per spin vs temperature for
+        lattice. Plot begins with T=0 configuration and plots to final_temp
+        in temp_step intervals.
+    '''
+    mag_vals = []
+    mag_vals_1 = []
+    mag_vals_2 = []
+    temp_vals = []
+    global _h3d
+    print("Initializing lattice...")
+    _h3d.initialize_lattice()
+    results = _h3d.M_v_B()
+    #print(results)
+
+    plt.subplot(311)
+    plt.plot(temp_vals, mag_vals, 'go')
+    plt.ylabel('$M$')
+    plt.xlabel('T')
+
+    plt.subplot(312)
+    plt.plot(temp_vals, mag_vals_1, 'go')
+    plt.ylabel('$M_{1}$')
+    plt.xlabel('T')
+
+    plt.subplot(313)
+    plt.plot(temp_vals, mag_vals_2, 'go')
+    plt.ylabel('$M_{2}$')
+    plt.xlabel('T')
+    plt.show()
+
+mag_v_temp(5, .1, 10000, 5000)
