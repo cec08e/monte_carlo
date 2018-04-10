1746352a7075f05a13d8bd5aa013f687ee164ad8
Caitlin Carnahan <caitlin@Caitlins-MBP.wv.cc.cmu.edu>
2018-04-10 17:42:35 -0400

    Sweeping K results for C code.

--- code/plot_h3d.py
+++ code/plot_h3d.py
@@ -13,7 +13,8 @@ _h3d = ctypes.CDLL('heisenberg3d.so')
 _h3d.initialize_lattice.argtypes = ()
 _h3d.M_v_B.argtypes = (ctypes.POINTER(PDOUBLE),)
 _h3d.M_v_B.restype = ctypes.c_int
-_h3d.mag_v_temp.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.c_int)
+_h3d.M_v_T.argtypes = (ctypes.POINTER(PDOUBLE), ctypes.c_double, ctypes.c_double, ctypes.c_double)
+_h3d.M_v_T.restype = ctypes.c_int
 _h3d.M_v_K.argtypes = (ctypes.POINTER(PDOUBLE),)
 _h3d.M_v_K.restype = ctypes.c_int
 
@@ -23,34 +24,50 @@ class Spin(ctypes.Structure):
 '''
 
 
-def mag_v_temp(init_temp = 0, final_temp = 1, temp_step = .1, eq_time = None, cor_time = None):
+def plot_M_v_T(init_temp = 0, final_temp = 1, temp_step = .1):
     ''' Plots the mean magnetization per spin vs temperature for
         lattice. Plot begins with T=0 configuration and plots to final_temp
         in temp_step intervals.
     '''
-    mag_vals = []
-    mag_vals_1 = []
-    mag_vals_2 = []
+    max_samples = 1000
+    M_vals = []
+    M1_vals = []
+    M2_vals = []
     temp_vals = []
     global _h3d
+
+    sample_arr = ctypes.c_double*5 # B, M, M1, M2 array for specific B value
+    data_arr = PDOUBLE*max_samples
+
+    results = data_arr()
+    for i in range(max_samples):
+        results[i] = sample_arr()
+
+
     print("Initializing lattice...")
     _h3d.initialize_lattice()
-    results = _h3d.mag_v_temp(5.0, .01, .05, 10000, 5000)
+    num_samples = _h3d.M_v_T(results, 5.0, .01, .05)
+
+    for i in range(num_samples):
+        temp_vals.append(results[i][0])
+        M_vals.append(results[i][4])
+        M1_vals.append(results[i][2])
+        M2_vals.append(results[i][3])
 
     plt.subplot(311)
-    plt.plot(temp_vals, mag_vals, 'go')
-    plt.ylabel('$M$')
+    plt.plot(temp_vals, M_vals, 'g')
+    plt.ylabel('$M1 - M2$')
     plt.xlabel('T')
 
     plt.subplot(312)
-    plt.plot(temp_vals, mag_vals_1, 'go')
+    plt.plot(temp_vals, M1_vals, 'g')
     plt.ylabel('$M_{1}$')
     plt.xlabel('T')
 
     plt.subplot(313)
-    plt.plot(temp_vals, mag_vals_2, 'go')
+    plt.plot(temp_vals, M2_vals, 'g')
     plt.ylabel('$M_{2}$')
-    plt.xlabel('T')
+    plt.xlabel('T (J_inter = .01, k = 1)')
     plt.show()
 
 def plot_M_v_B(max_samples = 500):
@@ -100,7 +117,7 @@ def plot_M_v_B(max_samples = 500):
     plt.plot(B_vals[int(num_samples/2):], M2_vals[int(num_samples/2):], 'b')
 
     plt.ylabel("$M_{2}$")
-    plt.xlabel("B")
+    plt.xlabel("B (J_inter = .1, K = .1)")
 
     plt.show()
 
@@ -151,11 +168,12 @@ def plot_M_v_K(max_samples = 500):
     plt.plot(K_vals[int(num_samples/2):], M2_vals[int(num_samples/2):], 'b')
 
     plt.ylabel("$M_{2}$")
-    plt.xlabel("K")
+    plt.xlabel("K ($J_inter = .1, B = .75$)")
 
     plt.show()
 
 if __name__ == "__main__":
     #mag_v_temp(5, .1, 10000, 5000)
-    plot_M_v_B()
-    #plot_M_v_K()
+    #plot_M_v_T(5, .1, .1)
+    #plot_M_v_B()
+    plot_M_v_K()
